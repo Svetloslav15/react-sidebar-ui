a32613af67cf242d0a96840628b960e386435b98
Svetloslav15 <sn_novoselski@abv.bg>
2020-05-21 19:42:26 +0300

    make nav collapse and add animations

--- example/src/App.js
+++ example/src/App.js
@@ -1,36 +1,40 @@
 import React from 'react'
-
 import {Sidebar, InputItem, DropdownItem, Icon, Item, Logo, LogoText} from 'react-sidebar-ui'
-import 'react-sidebar-ui/dist/index.css'
+import 'react-sidebar-ui/dist/index.css';
+import {Link} from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Sidebar bgColor='black'>
+      <Sidebar bgColor='black' isCollapsed={false}>
         <Logo
           image='https://media2.giphy.com/media/eNAsjO55tPbgaor7ma/source.gif'
           imageName='react logo'/>
         <LogoText>React Sidebar UI</LogoText>
 
         <DropdownItem values={['First', 'Second', 'Third']} bgColor={'black'}>Menu</DropdownItem>
+        <Link to='/home'>
+          <Item bgColor='black'>
+            <Icon><i className="fas fa-home"/></Icon>
+            Home
+          </Item>
+        </Link>
+        <Link to='/about'>
+          <Item bgColor='black'>
+            <Icon><i className="fas fa-info"/></Icon>
+            About
+          </Item>
+        </Link>
         <Item bgColor='black'>
-          <Icon><i className="fas fa-home"/></Icon>
-          Home
-        </Item>
-        <Item bgColor='black'>
-          <Icon><i className="fas fa-home"/></Icon>
-          About
-        </Item>
-        <Item bgColor='black'>
-          <Icon><i className="fas fa-home"/></Icon>
+          <Icon><i className="fas fa-sitemap"/></Icon>
           My Website
         </Item>
         <Item bgColor='black'>
-          <Icon><i className="fas fa-arrow-alt-circle-right"/></Icon>
+          <Icon><i className="far fa-address-book"/></Icon>
           Contacts
         </Item>
         <Item bgColor='black'>
-          <Icon><i className="fas fa-home"/></Icon>
+          <Icon><i className="fas fa-rss-square"/></Icon>
           Blog
         </Item>
       </Sidebar>
