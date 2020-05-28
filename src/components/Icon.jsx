723e8e55882fc95308f262551c4be6e8f5fed1d0
Svetloslav15 <sn_novoselski@abv.bg>
2020-05-28 18:14:04 +0300

    change icon comp ot be div

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Icon = ({children, styles, classes}) => {
   return (
-    <i className={'_2Vept ' + classes} style={{styles}}>{children}</i>
+    <div className={'_2Vept ' + classes} style={{styles}}>{children}</div>
   )
 };
 
